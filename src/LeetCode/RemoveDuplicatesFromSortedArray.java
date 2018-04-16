5ca91f86b5185995bbcb8b346c8f14942a59388d
sm880508@gmail.com <sm880508@gmail.com>
2018-04-17 00:18:23 +0800

    RemoveDuplicatesFromSortedArray

---
+++ src/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+/**
+ * Created by yiwei on 2018/4/16.
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+
+
+    public static int removeDuplicates(int[] nums) {
+
+
+        if(nums.length==0 )return 0;
+        int i=0 ;
+
+        for (int j = 1 ; j < nums.length ; j++){
+
+            if ( nums[i] != nums[j] ){
+
+                    i++;
+                nums[i] = nums[j];
+
+            }
+
+        }
+
+        return i+1 ;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@
  * Created by yiwei on 2018/4/12.
  */
 import LeetCode.*;
+
+import java.util.Arrays;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-        //System.out.println(MergeTwoSortedLists.mergeTwoLists();
+
+
+
+        System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(Arrays.stream(args).mapToInt(Integer::parseInt).toArray()));
 
     }
 }
