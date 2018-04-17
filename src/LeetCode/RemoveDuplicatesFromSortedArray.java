6d809f649b9f7952e191118dac85a663daef611a
yiwei1116 <sm880508@gmail.com>
2018-04-17 15:09:16 +0800

    modify Remove Duplicate Array

--- src/LeetCode/RemoveDuplicatesFromSortedArray.java
+++ src/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+
+
 /**
  * Created by yiwei on 2018/4/16.
  */
@@ -9,6 +11,13 @@ public class RemoveDuplicatesFromSortedArray {
 
     public static int removeDuplicates(int[] nums) {
 
+
+/*        int i = nums.length > 0 ? 1 : 0;
+        for (int n : nums)
+            if (n > nums[i - 1])
+                nums[i++] = n;
+        return i;
+    }*/
 
         if(nums.length==0 )return 0;
         int i=0 ;
