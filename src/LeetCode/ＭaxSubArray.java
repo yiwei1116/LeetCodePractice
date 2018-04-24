6e8837f9f3d389fdef5b8d4c7e6122364e094c1e
sm880508@gmail.com <sm880508@gmail.com>
2018-04-25 00:01:13 +0800

    Max sub array _My answer

--- src/LeetCode/ＭaxSubArray.java
+++ src/LeetCode/ＭaxSubArray.java
@@ -7,38 +7,44 @@ public class ＭaxSubArray {
         int max = nums[0];
         int Lsum = 0;
         int Rsum = 0;
-        int LL = 0 ;
-        int RR = 0 ;
+        int LM = 0 ;
+        int RM = 0 ;
         int flag = 0;
         if (nums == null || nums.length == 0 ) return 0;
         for (int i = 1 ; i < nums.length ; i++ ){
 
-            if (max < nums[i])
+            if (max < nums[i]){
                 max = nums [i];
                 flag = i ;
+            }
         }
         int left = flag - 1;
         int right = flag + 1 ;
         for (;left >= 0 ; left-- ){
 
-            if (Lsum+nums[left]>Lsum ) {
-                Lsum = Lsum + nums[left];
-                LL = left;
+
+            if (Lsum+nums[left]> LM ) {
+
+                LM = Lsum + nums[left];
             }
+            Lsum = Lsum + nums[left];
         }
         for ( ; right < nums.length ; right++){
 
-            if (Rsum+nums[right] >Rsum ) {
-                Rsum = Rsum + nums[right];
-                RR = right;
+            if (Rsum+nums[right] > RM ) {
+
+                RM = Rsum + nums[right];
             }
+            Rsum = Rsum + nums[right];
 
         }
-        if (Rsum < 0 && Lsum < 0 )
+        if (RM < 0 && LM < 0 )
             return max;
-        else if (Rsum < 0)
-            return max + Lsum;
-        else return max + Rsum;
+        else if (RM >= 0 && LM >= 0 )
+            return max + RM + LM;
+        else if (RM < 0)
+            return max + RM;
+        else return max + LM;
 
 
 
