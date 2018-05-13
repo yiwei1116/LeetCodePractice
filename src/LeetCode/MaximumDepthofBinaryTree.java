4c595553e4be2715ab90cfbc874beec3a0d81ec2
Yiwei1116 <sm880508@gmail.com>
2018-05-13 20:32:00 +0800

    iter

--- src/LeetCode/MaximumDepthofBinaryTree.java
+++ src/LeetCode/MaximumDepthofBinaryTree.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Stack;
+
 /**
  * Created by yiwei on 2018/5/10.
  */
@@ -28,4 +30,48 @@ public class MaximumDepthofBinaryTree {
     }
 
 
-}
+    /**
+     *          DFS  : Two stack to record  => 1. Node
+     *                                                      2. level
+     *
+     *
+     */
+      public static int maxDepthV2(TreeNode root) {
+
+          Stack<TreeNode> stack = new Stack<>();
+          Stack<Integer> value = new Stack<>();
+          stack.push(root);
+          value.push(1);
+          int max = 0;
+          while (!stack.isEmpty()) {
+
+              TreeNode node = stack.pop();
+              int temp = value.pop();
+              max = Math.max(temp, max);
+              if (node.left != null) {
+
+                  stack.push(node.left);
+                  value.push(temp + 1);
+              }
+              if (node.right != null) {
+
+                  stack.push(node.right);
+                  value.push(temp + 1);
+              }
+
+          }
+
+          return max;
+      }
+
+
+          }
+
+
+
+
+
+
+
+
+
