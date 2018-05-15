2cc06409a28df83c2c20a17bde628ebf9b8ff78e
sm880508@gmail.com <sm880508@gmail.com>
2018-05-16 01:31:54 +0800

    PascalsTriangle

--- src/LeetCode/PascalsTriangle.java
+++ src/LeetCode/PascalsTriangle.java
@@ -7,24 +7,38 @@ public class PascalsTriangle {
     public static List<List<Integer>> gernerate(int numRows){
 
 
-        List<Integer> levelList = new ArrayList<Integer>();
+
         List<List<Integer>> triangleList = new ArrayList<List<Integer>>();
 
-        if (numRows == 0) return null;
-        if (numRows == 1 ) {
-            levelList.add(1);
-            triangleList.add(new ArrayList(levelList));
-            return triangleList;
+        if (numRows == 0) return triangleList;
+
+        // First Row is always [1]
+        triangleList.add(new ArrayList<>());
+        triangleList.get(0).add(1);
+
+        for(int rowNum = 1 ; rowNum < numRows ; rowNum++){
+
+            List<Integer> row = new ArrayList<Integer>();
+            List<Integer> PrevRow = triangleList.get(rowNum-1);
+
+            // The first element is always 1
+            row.add(1);
+
+
+
+            for (int j=1 ; j < rowNum ; j++){
+
+                row.add(PrevRow.get(j-1)+PrevRow.get(j));
+
+            }
+
+            // The last element is always 1
+            row.add(1);
+            triangleList.add(row);
+
         }
-        if (numRows == 2 ){
 
-            levelList.add(1);
-            levelList.add(1);
-            triangleList.add(new ArrayList(levelList));
-            return triangleList;
-        }
+        return triangleList;
 
-
-        return null;
     }
 }
