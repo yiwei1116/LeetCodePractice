a8aa3f611f8c77937558a004b7d01158ca44e5d9
yiwei1116 <sm880508@gmail.com>
2018-05-17 21:40:41 +0800

    Find single value

---
+++ src/LeetCode/SingleValue.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+public class SingleValue {
+    /**
+     * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+     *
+     * Note:
+     * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     *
+     * Example 1:
+     * Input: [2,2,1]
+     * Output: 1
+     *
+     * Example 2:
+     * Input: [4,1,2,1,2]
+     * Output: 4
+     */
+    public static int singleNumber(int[] num){
+
+        int result = 0;
+        for (int i = 0 ; i < num.length ; i++){
+        /* XOR  will return 1 only on two different bit
+          So if two numbers are the same, XOR will return 0
+           Finally only one left
+
+           A ^ A = 0
+           0 ^ B = B
+            */
+        result = result ^ num[i];
+
+        }
+
+        return result;
+
+
+
+
+    }
+}
