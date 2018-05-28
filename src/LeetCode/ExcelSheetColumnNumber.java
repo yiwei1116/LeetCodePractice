bffd3129effea4ce95078630ec7c437b150a4384
sm880508@gmail.com <sm880508@gmail.com>
2018-05-28 23:13:36 +0800

    temp

---
+++ src/LeetCode/ExcelSheetColumnNumber.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.HashMap;
+
+/**
+ * Created by yiwei on 2018/5/28.
+ */
+public class ExcelSheetColumnNumber {
+    public static int titleToNumber(String s){
+
+        HashMap transferTable = new HashMap();
+
+        for (int i = 1; i < 27 ; i++)
+        {
+            transferTable.put(Character.toChars(i+64),i);
+
+        }
+
+        while ()
+
+
+
+
+
+
+        return 0;
+    }
+
+
+}
