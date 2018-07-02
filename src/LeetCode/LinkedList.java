11524dc29f9ea8ee8671d7a91e9e8b6e666fd330
sm880508@gmail.com <sm880508@gmail.com>
2018-07-02 20:40:08 +0800

    sum as LinkedList

--- src/LeetCode/LinkedList.java
+++ src/LeetCode/LinkedList.java
@@ -1,11 +1,15 @@
 package LeetCode;
 
+import java.security.PublicKey;
+import java.util.HashSet;
+
+
 /**
  * Created by yiwei on 2018/6/29.
- */class LinkedList
+ */public class LinkedList
 {
     //Class variables for the Linked List
-    private static Node head;
+    private  Node head;
     private static int numNodes;
 
     public static void main(String [] args)
@@ -17,38 +21,124 @@ package LeetCode;
         ll.addAtHead(13);
         ll.addAtTail(8);
         ll.addAtTail(7);
+        ll.addAtHead(11);
         ll.addAtIndex(4,9);
         ll.addAtIndex(4,9);
-        ll.deleteAtIndex(4);
+
         ll.printList();
-    }
+        System.out.println("=======");
 
-    public LinkedList(Object dat)
-    {
-        head = new Node(dat);
     }
+    public static LinkedList sumAsLinkedList(LinkedList l1, LinkedList l2){
 
-    public void addAtHead(Object dat)
-    {
+        if (l1 == null || l2 == null)return null;
+        LinkedList sumll = null;
+        Node h1 = l1.getHead();
+        Node h2 = l2.getHead();
+        int c = 0;
+        while (h1!=null && h2!=null)
+        {
+            int mod = (c + h1.getData() + h2.getData());
+
+            if ( mod > 9){
+                 mod = mod % 10;
+
+                c = 1;
+            }
+            else {
+
+                c = 0;
+            }
+            if (sumll==null)sumll = new LinkedList(mod);
+            else  sumll.addAtTail(mod);
+            h1 = h1.next;
+            h2 = h2.next;
+        }
+
+        while (h1!= null){
+
+            sumll.addAtTail(c+h1.getData());
+            h1 = h1.next;
+            c = 0 ;
+
+        }
+        while (h2!= null){
+
+            sumll.addAtTail(c+h2.getData());
+            h2 = h2.next;
+            c = 0;
+        }
+        if (c==1)
+            sumll.addAtTail(1);
+        return sumll;
+
+    }
+    public void printDoubleLinkedList(){
         Node temp = head;
+        while ( temp.next != null ) temp = temp.next;
+
+        while (temp!= null)
+        {
+            System.out.println(temp.getData());
+            temp = temp.prev;
+
+        }
+
+
+
+    }
+    public void DeleteMidNode(Node dNode){
+        Node temp = head;
+        if (temp == null)return ;
+        while (temp.next != null){
+            if (temp.next == dNode && temp.next.next != null){
+                temp.next = temp.next.next ;
+                return;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public LinkedList(int dat)
+    {
         head = new Node(dat);
-        head.next = temp;
+    }
+
+    public void addAtHead(int dat)
+    {
+        Node temp = new Node(dat);
+        if (head == null) head = temp;
+
+        else
+        {
+            temp.next = head;
+            head = temp;
+        }
         numNodes++;
     }
-
-    public void addAtTail(Object dat)
+    // Double linked list function
+    public void addAtTail(int dat)
     {
+
+
+
+        if (head ==null)
+        {
+            head = new Node(dat);
+            return;
+        }
         Node temp = head;
+        Node newNode = new Node(dat);
         while(temp.next != null)
         {
             temp = temp.next;
         }
-
-        temp.next = new Node(dat);
+        temp.next = newNode;
+        newNode.prev = temp;
         numNodes++;
     }
 
-    public void addAtIndex(int index, Object dat)
+    public void addAtIndex(int index, int dat)
     {
         Node temp = head;
         Node holder;
@@ -72,22 +162,101 @@ package LeetCode;
         temp.next = temp.next.next;
         numNodes--;
     }
+    public Node deleteNode(Node head, int i){
 
-    public static int find(Node n)
-    {
-        Node t = head;
-        int index = 0;
-        while(t != n)
-        {
-            index++;
-            t = t.next;
+        Node temp = head;
+
+        if ((int)temp.getData()== i )
+            return head.next;
+        while (temp.next!=null){
+
+            if ((int)temp.next.getData()==i){
+
+                temp.next = temp.next.next;
+                return head;
+            }
+            temp = temp.next;
         }
-        return index;
+
+        return head;
+
+
+
+    }
+    public void deleteDuplicates(Node node){
+        Node prev = null;
+        HashSet checkT = new HashSet();
+        while (node != null){
+            if (checkT.contains(node.data)){
+                prev.next = node.next;
+            }else {
+                checkT.add(node.data);
+                prev = node;
+            }
+
+                node = node.next;
+
+        }
+
+    }
+    //  1 2 3 4 5 6 7 null
+    //if k = 2, return node 5
+    // two pointer from first we need to count k-2 element
+
+    public Node findKthToLastElement(Node head, int index){
+        Node curr = head;
+        Node follower = head;
+
+
+        for (int i = 0 ; i < index ; i++){
+
+            if (curr == null )return null;
+
+            curr = curr.next;
+        }
+        while (curr.next!=null)
+        {
+            curr = curr.next;
+            follower = follower.next;
+        }
+        return follower;
+
+
+
+    }
+    public void reverseLinkedList(){
+
+        Node cur = head;
+        Node prev = null;
+        Node next ;
+        while (cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+
+        }
+        head = prev;
+
+    }
+    public void recurReverseLinkedList(Node temp){
+
+
+        if (temp.next == null)
+        {
+            head = temp;
+            return;
+        }
+        recurReverseLinkedList(temp.next);
+        Node q = temp.next;
+        q.next = temp;
+        temp.next = null;
+
     }
 
-    public static Node find(int index)
+    public Node find(int index)
     {
-        Node temp=head;
+        Node temp= head;
         for(int i=0; i<index; i++)
         {
             temp = temp.next;
@@ -95,7 +264,7 @@ package LeetCode;
         return temp;
     }
 
-    public static void printList()
+    public  void printList()
     {
         Node temp = head;
         while(temp != null)
@@ -104,26 +273,40 @@ package LeetCode;
             temp = temp.next;
         }
     }
+    public static void recursionReversePrint(Node node){
+
+        if (node.next != null)
+            recursionReversePrint(node.next);
+
+
+        System.out.println(node.getData());
+
+
+    }
+    public Node getHead(){
+
+        return head;
+    }
 
     public static int getSize()
     {
         return numNodes;
     }
 
-    class Node
+    public class Node
     {
         //Declare class variables
-        private Node next;
-        private Object data;
-
-        public Node(Object dat)
+        private Node next = null;
+        private int data;
+        private Node prev =  null;
+        public Node(int dat)
         {
             data = dat;
         }
 
-        public Object getData()
+        public int getData()
         {
-            return data;
+            return  data;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import LeetCode.*;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-
+import LeetCode.*;
 public class Main {
 
 
@@ -14,17 +14,30 @@ public class Main {
         char[] Str = St.toCharArray();
         int[] num1 = {1,2,3,4,5,6};
         int[] num2 = {2,5,6};
-
-
-        UserLinkedList Ull = new UserLinkedList(10);
-        Ull.addAtHead(9);
-        Ull.addAtHead(8);
-        Ull.addAtTail(7);
-        Ull.addAtIndex(2,15);
-        UserLinkedList.Node Un = Ull.find(2);
-        System.out.print(Un.getData());
-        //Ull.printList();
-
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        LeetCode.LinkedList ll = new LeetCode.LinkedList(2);
+        ll.addAtTail(8);
+        ll.addAtTail(7);
+        ll.addAtTail(6);// 2 8 7 6 5 4 3
+        ll.addAtTail(5);// 5 0 1 5 6 3 7 5 4
+        ll.addAtTail(4);// 7 8 8 1 2 8 0 6 4
+        ll.addAtTail(3);
+        ll.printList();
+        System.out.println("----------LL2------------");
+        LeetCode.LinkedList ll2 = new LeetCode.LinkedList(5);
+        ll2.addAtTail(0);
+        ll2.addAtTail(1);
+        ll2.addAtTail(5);
+        ll2.addAtTail(6);
+        ll2.addAtTail(3);
+        ll2.addAtTail(7);
+        ll2.addAtTail(5);
+        ll2.addAtTail(4);
+        ll2.printList();
+        LeetCode.LinkedList lsum = new LeetCode.LinkedList(0) ;
+        lsum = LeetCode.LinkedList.sumAsLinkedList(ll,ll2);
+        System.out.println("=======");
+        lsum.printList();
         //System.out.println(RemoveDuplicatesFromSortedArray.removeDuplicates(Arrays.stream(args).mapToInt(Integer::parseInt).toArray()));
         //System.out.println(URLify.URLsify(Str,9));
     }
